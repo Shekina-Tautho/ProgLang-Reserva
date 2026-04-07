d28df9ac0d0e0e723a9fad6eb07f156184b9d5d6
Shekina-Tautho <supagwapashikyna@gmail.com>
2026-04-08 00:47:06 +0800

    implemented register()

--- auth.py
+++ auth.py
@@ -1,19 +1,24 @@
 import csv
+from pathlib import Path
 
 filename = "users.csv"
 filepath = 'files/users.csv'
+path = Path(filepath)
 
 fields = []
 rows = []
 role = "user"
 found = False
 
+uname = ""
+pword = ""
+
 
 def create_csv():
-    data = ['username', 'password', 'role']
     csv_file_path = 'files/users.csv'
     
-    with open(csv_file_path, mode='w', newline='') as file:
+    with open(csv_file_path, 'w', newline='') as file:
+        data = [['username', 'password', 'role']]
         writer = csv.writer(file)
         writer.writerows(data)
 
@@ -26,30 +31,62 @@ def load_csv():
          for row in csvreader:
              rows.append(row)
      return rows
-    
-          
+
+def add_record():
+    data = [[uname, pword, role]]
+    with open(filepath, 'a', newline='') as file:
+       writer = csv.writer(file)
+       writer.writerows(data)
+           
     
 def login():
     print("Welcome to the login page!")
     username = input("Please enter your username: ")
     password = input("Please enter your password: ")
-    load_csv()
-    for row in rows:
-        if (row[0] == username) and (row[1] == password):
-            global found
-            found = True
-            break
-        
-    if found:
-        print("Successfully logged in.")
+    
+    global found
+    found = False
+    if path.exists():
+        global rows
+        rows = []
+        load_csv()
+        for row in rows:
+            if (row[0] == username) and (row[1] == password):
+                found = True
+                break
+            
+        if found:
+            print("Successfully logged in.")
+        else:
+            print("Login attempt unsuccessful.")
     else:
-        print("Login attempt unsuccessful.")
+        create_csv()
             
 
-    
     
 def register():
     print("Welcome to the register page!")
     username = input("Please enter the username you want to use: ")
     password = input("Please enter the password you want to use: ")
     
+    global found 
+    found = False
+    if path.exists():
+        global rows
+        rows = []
+        load_csv()
+        for row in rows:
+            if (row[0] == username):
+                found = True
+                break
+        
+        if found:
+            print("An account with this username already exists.")
+        else:
+            global uname, pword
+            uname = username
+            pword = password
+            add_record()
+            
+    else:
+        create_csv()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-sys.stdout.reconfigure(encoding='utf-8')
 from auth import login, register
+sys.stdout.reconfigure(encoding='utf-8')
 
 def show_logo():
     print(r"""
